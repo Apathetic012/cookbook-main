b751f67e3aeae67c5e71a2572a8401412ae3603e
Cyrus David <david@jcyr.us>
2013-04-28 13:02:46 +0800

    use bash for heroku installer

--- recipes/heroku_toolbelt.rb
+++ recipes/heroku_toolbelt.rb
@@ -1,6 +1,6 @@
 if node[:vagrant_main][:install_heroku]
   execute "install heroku toolbelt" do
-    command "wget -qO- https://toolbelt.heroku.com/install.sh | sh"
+    command "wget -qO- https://toolbelt.heroku.com/install.sh | /bin/bash"
     action :run
     not_if "which heroku"
   end
