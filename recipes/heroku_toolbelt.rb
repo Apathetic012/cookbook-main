0f70b756d9916c56cdf6303aba9d135aa9d7d076
Cyrus David <apathetic012@gmail.com>
2013-05-16 13:54:52 +0800

    change heroku install method

--- recipes/heroku_toolbelt.rb
+++ recipes/heroku_toolbelt.rb
@@ -1,6 +1,10 @@
 if node[:vagrant_main][:install_heroku]
-  execute "install heroku toolbelt" do
-    command "wget -qO- https://toolbelt.heroku.com/install.sh | /bin/bash"
+  bash "install heroku toolbelt" do
+    code <<-EOH
+      curl -s http://assets.heroku.com.s3.amazonaws.com/heroku-client/heroku-client.tgz | tar xz
+      mv heroku-client /opt
+      ln -s /opt/heroku-client/bin/heroku /usr/local/bin/heroku
+    EOH
     action :run
     not_if "which heroku"
   end
